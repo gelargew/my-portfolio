0fd92f4da7e9aa4fac3fc5c719cab6ba71ac6ce7
Gelar Rustiawan <gelargew@gmail.com>
2024-05-17 09:56:25 +0700

    init portfolio

---
+++ src/pages/blog.js
@@ -0,0 +1,38 @@
+export default function Blog() {
+    return (
+        <main>
+            <h1>Ini adalah halaman Blog</h1>
+            <br />
+            <hr />
+            <ul>
+                {BLOGS.map((blog, i) => (
+                    <li key={i}>
+                        <h2>{blog.title}</h2>
+                        <p>{blog.content}</p>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    )
+}
+
+const BLOGS = [
+    {
+        title: 'Blog 1',
+        date: '2023-01-01',
+        content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.',
+    },
+    {
+        title: 'Blog 2',
+        date: '2023-01-01',
+        content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.',
+    },
+    {
+        title: 'Blog 3',
+        date: '2023-01-01',
+        content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.',
+    },
+]
